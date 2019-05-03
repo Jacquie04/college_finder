a1453910e077232c48325382759f684ff1196df0
Ritchie Garcia <detectives.salvajes@protonmail.com>
2019-05-02 21:04:50 -0700

    mvc file structure up and running

--- college-finder/server.js
+++ college-finder/server.js
@@ -1,18 +1,19 @@
 const express = require("express");
 const path = require("path");
+const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+//Handles routes. Will serve static react page if /api/x not utilized
+app.use(routes);
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
