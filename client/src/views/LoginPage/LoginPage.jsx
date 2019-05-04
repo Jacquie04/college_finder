05ce6e2d9e0ad383deca9dc1cf9736a80c0ee422
Jacquelyn Duley <45111833+Jacquie04@users.noreply.github.com>
2019-05-04 13:12:23 -0700

    login page loading now updates to come

---
+++ client/src/views/LoginPage/LoginPage.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+
+import withStyles from "@material-ui/core/styles/withStyles";
+
+//components for login page
+import Header from "../../assets/components/Header/Header.jsx";
+import HeaderLinks from "../../assets/components/Header/HeaderLinks.jsx";
+import Footer from "../../assets/components/Footer/Footer.jsx";
+import GridContainer from "../../assets/components/Grid/GridContainer.jsx";
+import GridItem from "../../assets/components/Grid/GridItem.jsx";
+import Button from "../../assets/components/CustomButtons/Button.jsx";
+import Card from "../../assets/components/Card/Card.jsx";
+import CardBody from "../../assets/components/Card/CardBody.jsx";
+import CardHeader from "../../assets/components/Card/CardHeader.jsx";
+import CardFooter from "../../assets/components/Card/CardFooter.jsx";
+import CustomInput from "../../assets/components/CustomInput/CustomInput.jsx";
+
+import loginPageStyle from "../../assets/jss/material-kit-react/views/loginPage.jsx";
+
+import image from "../../assets/img/college2.jpg";
+
+class LoginPage extends React.Component {
+    constructor(props) {
+        super(props);
+        // we use this to make the card to appear after the page has been rendered
+        this.state = {
+            cardAnimaton: "cardHidden"
+        };
+    }
+    componentDidMount() {
+        // we add a hidden class to the card and after 700 ms we delete it and the transition appears
+        setTimeout(
+            function () {
+                this.setState({ cardAnimaton: "" });
+            }.bind(this),
+            700
+        );
+    }
+    render() {
+        const { classes, ...rest } = this.props;
+        return (
+            <div>
+                <Header
+                    absolute
+                    color="transparent"
+                    brand="Material Kit React"
+                    rightLinks={<HeaderLinks />}
+                    {...rest}
+                />
+                <div
+                    className={classes.pageHeader}
+                    style={{
+                        backgroundImage: "url(" + image + ")",
+                        backgroundSize: "cover",
+                        backgroundPosition: "top center"
+                    }}
+                >
+                    <div className={classes.container}>
+                        <GridContainer justify="center">
+                            <GridItem xs={12} sm={12} md={4}>
+                                <Card className={classes[this.state.cardAnimaton]}>
+                                    <form className={classes.form}>
+                                        <CardHeader color="primary" className={classes.cardHeader}>
+                                            <h4>Login</h4>
+                                        </CardHeader>
+                                        <p className={classes.divider}>Or Be Classical</p>
+                                        <CardBody>
+                                            <CustomInput
+                                                labelText="First Name..."
+                                                id="first"
+                                                formControlProps={{
+                                                    fullWidth: true
+                                                }}
+            
+                                            />
+                                            <CustomInput
+                                                labelText="Email..."
+                                                id="email"
+                                                formControlProps={{
+                                                    fullWidth: true
+                                                }}
+                                            />
+                                            <CustomInput
+                                                labelText="Password"
+                                                id="pass"
+                                                formControlProps={{
+                                                    fullWidth: true
+                                                }}
+                                            />
+                                        </CardBody>
+                                        <CardFooter className={classes.cardFooter}>
+                                            <Button simple color="primary" size="lg">
+                                                Get started
+                        </Button>
+                                        </CardFooter>
+                                    </form>
+                                </Card>
+                            </GridItem>
+                        </GridContainer>
+                    </div>
+                    <Footer whiteFont />
+                </div>
+            </div>
+        );
+    };
+};
+
+
+export default withStyles (loginPageStyle)(LoginPage);
