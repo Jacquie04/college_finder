68d7f619d58fe82adfc48b97d9c407590b068d0d
Ritchie Garcia <detectives.salvajes@protonmail.com>
2019-05-18 10:51:42 -0700

    corrects app.js

--- college-finder/client/src/App.js
+++ college-finder/client/src/App.js
@@ -1,11 +1,70 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import LandingPage from "./views/LandingPage/LandingPage";  
-import LoginPage from "./views/LoginPage/LoginPage";
-import ProfilePage from "./views/ProfilePage/ProfilePage"; 
-import RegistrationPage from "./views/RegistrationPage/RegistrationPage"; 
+import React, { Component } from 'react';
+import { BrowserRouter as BrowserRouter, Router, Redirect, Route, Switch } from "react-router-dom";
+// import LandingPage from "./views/LandingPage";  <Route exact path="/home" component={LandingPage} />
+import Login from "./views/LoginPage/LoginPage";
+import Register from "./views/ProfilePage/ProfilePage"; 
+import Home from "./views/ProfilePage/ProfilePage"; 
 
+import "./App.css";
+import axios from "axios";
 
+function PrivateRoute({ user, component: Component, ...rest }) {
+  return (
+    <Route {...rest}
+      render={props =>
+        !!user ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              //we dont have users or password so just change this line to what page you are working on. 
+              pathname: "/login",
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+class App extends Component {
+  state = {
+    user: null
+  };
+
+  componentDidMount() {
+    axios.get('/api/user')
+      .then(res => {console.log(res);
+        // this.setState({ user: res.data.id }, () => {
+        //   this.props.history.push('/');
+        // });
+      }).catch(err => {
+        console.log('no user');
+      });
+  }
+
+  setUser = (res) => {
+      this.setState({ user: res.data.id }, () => {
+        this.props.history.push('/');
+      });
+  } 
+
+  render() {
+    return (
+      <BrowserRouter>
+      <Switch>
+        <Route path="/login" render={(props) => <Login {...props} setUser={this.setUser} /> } />
+        <Route path="/register" component={Register} setUser={this.setUser} />
+        <PrivateRoute path="/" exact component={Home} user={this.state.user} />
+        <Route render={() => <Redirect to="/login" />} />
+      </Switch>
+      </BrowserRouter>
+    );
+  }
+}
+
+/*
 const App = () => (
   <Router>
     <div>
@@ -16,7 +75,8 @@ const App = () => (
         <Route exact path="/signup" component={RegistrationPage} />
       </Switch>
     </div>
-  </Router>
+  </Router> 
 );
+*/
 
 export default App;
