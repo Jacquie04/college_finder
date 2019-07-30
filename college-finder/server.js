9376aa01cb17f85ea369b17a46101d55a6927d1b
Ritchie Garcia <detectives.salvajes@protonmail.com>
2019-07-30 12:56:03 -0700

    initialized .env

--- college-finder/server.js
+++ college-finder/server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const path = require("path");
 const routes = require("./routes");
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
+require('dotenv').config();
 const PORT = process.env.PORT || 3001;
 const app = express();
 var db = require("./models");
